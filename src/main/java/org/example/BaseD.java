123ba655da3e6b43947c5c34ca86802d35053fd6
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-12-10 09:28:19 +0000

    add in abstract

--- src/main/java/org/example/BaseD.java
+++ src/main/java/org/example/BaseD.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Embeddable;
  * Created on 03/12/2024 by Paul Harrison (paul.harrison@manchester.ac.uk).
  */
 @Embeddable
-public  class BaseD { //TODO would really like this to be abstract
+public abstract class BaseD { //TODO would really like this to be abstract
   public String baseprop;
 
    public BaseD(String baseprop) {
