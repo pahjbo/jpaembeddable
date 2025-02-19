a82652adf767b955baff61d5e1dfbcfb19844655
Paul Harrison <paul.harrison@manchester.ac.uk>
2025-02-19 11:28:37 +0000

    make work with hibernate 6.6+
    can use mapped superclass if the @embedded sites only use leaf nodes

--- src/main/java/org/example/DerOA.java
+++ src/main/java/org/example/DerOA.java
@@ -16,7 +16,7 @@ public class DerOA extends BaseO{
 //   @AttributeOverrides({
 //         @AttributeOverride(name="a",column = @Column(name = "da"))
 //   })
-   public BaseD derda;
+   public DerDA derda;
 
    public DerOA() {
 
